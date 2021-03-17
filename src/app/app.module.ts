a6f557a7765da20d8e30c0bfbec342671ca3e488
Mikhail Goloto <goloto@pm.me>
2021-03-17 22:41:02 +0300

    add Footer and Menu components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimerComponent
+    TimerComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  public year: number;
+
+  constructor() {
+    const date = new Date();
+    this.year = date.getFullYear();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
