bb41159cf482af003d057408c121335923837a6f
Mikhail Goloto <goloto@pm.me>
2021-03-17 22:40:14 +0300

    add new TimerTime entity, edit timer component styles and layout

---
+++ src/app/timer/TimerTime.ts
@@ -0,0 +1,83 @@
+export class TimerTime {
+  private _hours!: number;
+  private _minutes!: number;
+  private _seconds!: number;
+  private _formattedHours!: string;
+  private _formattedMinutes!: string;
+  private _formattedSeconds!: string;
+
+  constructor() {
+    this.updateTime();
+  }
+
+  private formatTime(num: number): string {
+    return num.toString().length > 1 ? num.toString() : '0' + num.toString();
+  }
+
+  public updateTime(): void {
+    const currentTime = new Date();
+    const isCurrentDay = currentTime.getHours() < 18;
+    const hourWhenWorkdayEnd = 18;
+    const hoursInDay = 24;
+    const secondsInMinute = 60;
+    const minutesInHour = 60;
+
+    this.minutes = minutesInHour - currentTime.getMinutes();
+    this.seconds = secondsInMinute - currentTime.getSeconds();
+
+    if (+this.seconds > 0) {
+      this.minutes -= 1;
+    }
+
+    if (isCurrentDay) {
+      this.hours = 17 - currentTime.getHours();
+    } else {
+      this.hours = hoursInDay - 1 - currentTime.getHours() + hourWhenWorkdayEnd;
+    }
+  }
+
+  set hours(value: number) {
+    if (value >= 0) {
+      this._hours = value;
+      this._formattedHours = this.formatTime(value);
+    }
+  }
+
+  get hours(): number {
+    return this._hours;
+  }
+
+  set minutes(value: number) {
+    if (value >= 0) {
+      this._minutes = value;
+      this._formattedMinutes = this.formatTime(value);
+    }
+  }
+
+  get minutes(): number {
+    return this._minutes;
+  }
+
+  set seconds(value: number) {
+    if (value >= 0) {
+      this._seconds = value;
+      this._formattedSeconds = this.formatTime(value);
+    }
+  }
+
+  get seconds(): number {
+    return this._seconds;
+  }
+
+  get formattedHours(): string {
+    return this._formattedHours;
+  }
+
+  get formattedMinutes(): string {
+    return this._formattedMinutes;
+  }
+
+  get formattedSeconds(): string {
+    return this._formattedSeconds;
+  }
+}
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerTime } from './TimerTime';
 
 @Component({
   selector: 'app-timer',
@@ -6,41 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
-  public time!: {
-    minutes: number,
-    hours: number,
-    seconds: number
-  };
+  public time: TimerTime;
 
-  constructor() {}
+  constructor() {
+    this.time = new TimerTime();
+  }
 
   ngOnInit(): void {
     setInterval(() => {
-      this.time = {
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      };
-
-      const currentTime = new Date();
-      const isCurrentDay = currentTime.getHours() < 18;
-      const hourWhenWorkdayEnd = 18;
-      const hoursInDay = 24;
-      const secondsInMinute = 60;
-      const minutesInHour = 60;
-
-      this.time.minutes = minutesInHour - currentTime.getMinutes();
-      this.time.seconds = secondsInMinute - currentTime.getSeconds();
-
-      if (this.time.seconds > 0) {
-        this.time.minutes -= 1;
-      }
-
-      if (isCurrentDay) {
-        this.time.hours = 18 - currentTime.getHours();
-      } else {
-        this.time.hours = hoursInDay - currentTime.getHours() + hourWhenWorkdayEnd;
-      }
+      this.time.updateTime();
     }, 1000);
   }
 
