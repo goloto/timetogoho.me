3767d206e37850fe1c497ef04392c9ef8a828539
Mikhail Goloto <goloto@pm.me>
2021-03-20 00:44:48 +0300

    add hours calculating to start and end of working day

--- src/app/timer/TimerTime.ts
+++ src/app/timer/TimerTime.ts
@@ -1,3 +1,5 @@
+import {TimeFunctions} from "./TimeFunctions";
+
 export class TimerTime {
   private _hours!: number;
   private _minutes!: number;
@@ -5,41 +7,44 @@ export class TimerTime {
   private _formattedHours!: string;
   private _formattedMinutes!: string;
   private _formattedSeconds!: string;
+  private _dayStartHours = 9;
+  private _dayStartMinutes = 0;
+  private _dayEndHours = 18;
+  private _dayEndMinutes = 0;
+  private _isWorkingDay = false;
 
   constructor() {
     this.updateTime();
   }
 
-  private formatTime(num: number): string {
-    return num.toString().length > 1 ? num.toString() : '0' + num.toString();
+  private isWorkingHour(hour: number): boolean {
+    const hoursUntilEnd = TimeFunctions.calcHoursDifferent(hour, this.dayEndHours);
+    const workingDayLength = TimeFunctions.calcHoursDifferent(this.dayStartHours, this.dayEndHours);
+
+    return (hoursUntilEnd <= workingDayLength);
   }
 
   public updateTime(): void {
     const currentTime = new Date();
-    const isCurrentDay = currentTime.getHours() < 18;
-    const hourWhenWorkdayEnd = 18;
-    const hoursInDay = 24;
     const secondsInMinute = 60;
-    const minutesInHour = 60;
+    const incompletePiece = 1;
+    this.isWorkingDay = this.isWorkingHour(currentTime.getHours());
 
-    this.minutes = minutesInHour - currentTime.getMinutes();
-    this.seconds = (secondsInMinute - 1) - currentTime.getSeconds();
-
-    if (this.seconds >= 0) {
-      this.minutes -= 1;
-    }
-
-    if (isCurrentDay) {
-      this.hours = (hourWhenWorkdayEnd - 1) - currentTime.getHours();
+    if (this.isWorkingHour(currentTime.getHours())) {
+      this.hours = TimeFunctions.calcHoursDifferent(currentTime.getHours(), this.dayEndHours) - incompletePiece;
+      this.minutes = TimeFunctions.calcMinutesDifferent(currentTime.getMinutes(), this.dayEndMinutes) - incompletePiece;
     } else {
-      this.hours = (hoursInDay - 1) - currentTime.getHours() + hourWhenWorkdayEnd;
+      this.hours = TimeFunctions.calcHoursDifferent(currentTime.getHours(), this.dayStartHours) - incompletePiece;
+      this.minutes = TimeFunctions.calcMinutesDifferent(currentTime.getMinutes(), this.dayStartMinutes) - incompletePiece;
     }
+
+    this.seconds = (secondsInMinute - incompletePiece) - currentTime.getSeconds();
   }
 
   set hours(value: number) {
     if (value >= 0) {
       this._hours = value;
-      this._formattedHours = this.formatTime(value);
+      this._formattedHours = TimeFunctions.convertToBinaryString(value);
     }
   }
 
@@ -50,7 +55,7 @@ export class TimerTime {
   set minutes(value: number) {
     if (value >= 0) {
       this._minutes = value;
-      this._formattedMinutes = this.formatTime(value);
+      this._formattedMinutes = TimeFunctions.convertToBinaryString(value);
     }
   }
 
@@ -61,7 +66,7 @@ export class TimerTime {
   set seconds(value: number) {
     if (value >= 0) {
       this._seconds = value;
-      this._formattedSeconds = this.formatTime(value);
+      this._formattedSeconds = TimeFunctions.convertToBinaryString(value);
     }
   }
 
@@ -79,5 +84,53 @@ export class TimerTime {
 
   get formattedSeconds(): string {
     return this._formattedSeconds;
+  }
+
+  set dayStartHours(value: number) {
+    if (value >= 0 && value < 24) {
+      this._dayStartHours = value;
+    }
+  }
+
+  get dayStartHours(): number {
+    return this._dayStartHours;
+  }
+
+  set dayStartMinutes(value: number) {
+    if (value >= 0 && value < 60) {
+      this._dayStartMinutes = value;
+    }
+  }
+
+  get dayStartMinutes(): number {
+    return this._dayStartMinutes;
+  }
+
+  set dayEndHours(value: number) {
+    if (value >= 0 && value < 24) {
+      this._dayEndHours = value;
+    }
+  }
+
+  get dayEndHours(): number {
+    return this._dayEndHours;
+  }
+
+  set dayEndMinutes(value: number) {
+    if (value >= 0 && value < 60) {
+      this._dayEndMinutes = value;
+    }
+  }
+
+  get dayEndMinutes(): number {
+    return this._dayEndMinutes;
+  }
+
+  set isWorkingDay(value: boolean) {
+    this._isWorkingDay = value;
+  }
+
+  get isWorkingDay(): boolean {
+    return this._isWorkingDay;
   }
 }
