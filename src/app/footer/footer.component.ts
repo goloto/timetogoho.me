830548b6067a26b951973911b3b79dea96eb8b5b
Mikhail Goloto <goloto@pm.me>
2021-03-30 22:48:42 +0300

    add hidden footer menu and its animation

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,19 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-footer',
+  animations: [
+    trigger('isMenuOpen', [
+      state('closed', style({
+        height: '0',
+      })),
+      state('open', style({
+        height: '6vh',
+      })),
+      transition('open <=> closed', [
+        animate('0.2s')
+      ]),
+    ]),
+    trigger('isArrowsUp', [
+      state('up', style({
+        transform: 'rotate(-135deg)',
+      })),
+      state('down', style({
+        transform: 'rotate(45deg)',
+      })),
+      transition('up <=> down', [
+        animate('0.2s')
+      ]),
+    ]),
+  ],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  public year: number;
+  public currentYear: number;
+  public isMenuOpen = false;
 
   constructor() {
-    const date = new Date();
-    this.year = date.getFullYear();
+    const currentDate = new Date();
+    this.currentYear = currentDate.getFullYear();
   }
 
   ngOnInit(): void {
   }
 
+  toggleMenuVisibility(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
