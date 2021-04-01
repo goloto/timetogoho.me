4ee31a100b397ae74135fe467c9251c9e8fa6c38
Mikhail Goloto <goloto@pm.me>
2021-04-01 20:56:46 +0300

    create header-menu component as mirrored footer-menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
+  animations: [
+    trigger('isMenuOpen', [
+      state('closed', style({
+        height: '0',
+      })),
+      state('open', style({
+        height: '*',
+      })),
+      transition('open <=> closed', [
+        animate('0.2s')
+      ]),
+    ]),
+    trigger('isArrowsUp', [
+      state('up', style({
+        transform: 'rotate(-135deg)',
+      })),
+      state('down', style({
+        transform: 'rotate(45deg)',
+      })),
+      transition('up <=> down', [
+        animate('0.2s')
+      ]),
+    ]),
+    trigger('isArrowReversed', [
+      state('not-reversed', style({
+        flexDirection: 'column',
+      })),
+      state('reversed', style({
+        flexDirection: 'column-reverse',
+      })),
+    ]),
+    trigger('isHeaderMenuExpanded', [
+      state('expanded', style({
+        boxShadow: '0vh -.1vh 1vh gray',
+      })),
+      state('not-expanded', style({
+        boxShadow: 'none',
+      })),
+    ]),
+  ],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  public isMenuOpen = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleMenuVisibility(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
-
 }
