7cfb3b4a04dd5ed1f9d4261fa193ab7a73e0bffa
Mikhail Goloto <goloto@pm.me>
2021-04-24 10:26:35 +0300

    add new colors to color scheme; rebuild menu elements list

--- src/app/menu/on-menu-open-animations.ts
+++ src/app/menu/on-menu-open-animations.ts
@@ -4,9 +4,11 @@ export const onMenuOpenAnimations = [
   trigger('isMenuOpen', [
     state('closed', style({
       height: '0',
+      padding: '0',
     })),
     state('open', style({
       height: '*',
+      padding: '4px',
     })),
     transition('open <=> closed', [
       animate('0.2s')
