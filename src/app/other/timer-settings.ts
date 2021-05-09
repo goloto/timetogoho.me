17760f9321d9763993f65d31143c183d1e6294e1
Mikhail Goloto <goloto@pm.me>
2021-05-09 15:53:51 +0300

    clear old not-used TimerSettings methods and variables

--- src/app/other/timer-settings.ts
+++ src/app/other/timer-settings.ts
@@ -5,10 +5,6 @@ import { Time } from './time';
   providedIn: 'root',
 })
 export class TimerSettings {
-  private _dayStartHours = 9;
-  private _dayStartMinutes = 0;
-  private _dayEndHours = 18;
-  private _dayEndMinutes = 0;
   private _isWorkingDay = false;
 
   private _dayStart = new Time(0, 0, 0);
@@ -28,46 +24,6 @@ export class TimerSettings {
 
   get dayEnd(): Time {
     return this._dayEnd;
-  }
-
-  set dayStartHours(value: number) {
-    if (value >= 0 && value < 24) {
-      this._dayStartHours = value;
-    }
-  }
-
-  get dayStartHours(): number {
-    return this._dayStartHours;
-  }
-
-  set dayStartMinutes(value: number) {
-    if (value >= 0 && value < 60) {
-      this._dayStartMinutes = value;
-    }
-  }
-
-  get dayStartMinutes(): number {
-    return this._dayStartMinutes;
-  }
-
-  set dayEndHours(value: number) {
-    if (value >= 0 && value < 24) {
-      this._dayEndHours = value;
-    }
-  }
-
-  get dayEndHours(): number {
-    return this._dayEndHours;
-  }
-
-  set dayEndMinutes(value: number) {
-    if (value >= 0 && value < 60) {
-      this._dayEndMinutes = value;
-    }
-  }
-
-  get dayEndMinutes(): number {
-    return this._dayEndMinutes;
   }
 
   set isWorkingDay(value: boolean) {
