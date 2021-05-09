e4c5039b0907f225cf6b1ac742425233f6f80f1e
Mikhail Goloto <goloto@pm.me>
2021-05-09 13:04:03 +0300

    fix calcTimeDifferent work; delete old timerDifferent functions

--- src/app/other/time-functions.ts
+++ src/app/other/time-functions.ts
@@ -7,44 +7,30 @@ export class TimeFunctions {
     return num.toString().length > 1 ? num.toString() : `0${num.toString()}`;
   }
 
-  static calcHoursDifferent(startHour: number, endHour: number): number {
-    if (endHour > startHour) {
-      return endHour - startHour;
-    } else {
-      const hoursInDay = 24;
-
-      return hoursInDay - startHour + endHour;
-    }
-  }
-
-  static calcMinutesDifferent(startMinutes: number, endMinutes: number): number {
-    if (endMinutes > startMinutes) {
-      return endMinutes - startMinutes;
-    } else {
-      const minutesInHour = 60;
-
-      return minutesInHour - startMinutes + endMinutes;
-    }
-  }
-
   static calcTimeDifferent(startTime: Time, endTime: Time): Time {
     const resultTime = new Time(0, 0 , 0);
-    let isHourUnfull = false;
-
-    if (startTime.minutes > endTime.minutes) {
-      isHourUnfull = true;
-      resultTime.minutes = 60 - startTime.minutes + endTime.minutes - 1;
-    } else {
-      resultTime.minutes = endTime.minutes - startTime.minutes - 1;
-    }
 
     if (startTime.hours > endTime.hours) {
-      resultTime.hours = 24 - startTime.hours + endTime.hours - (isHourUnfull ? 1 : 0);
+      resultTime.hours = 24 - startTime.hours + endTime.hours;
     } else {
-      resultTime.hours = endTime.hours - startTime.hours - (isHourUnfull ? 1 : 0);
+      resultTime.hours = endTime.hours - startTime.hours;
     }
 
-    resultTime.seconds = startTime.seconds - endTime.seconds - 1;
+    if (startTime.minutes > endTime.minutes) {
+      resultTime.reduceHour();
+
+      resultTime.minutes = 60 - startTime.minutes + endTime.minutes;
+    } else {
+      resultTime.minutes = endTime.minutes - startTime.minutes;
+    }
+
+    if (startTime.seconds > endTime.seconds) {
+      resultTime.reduceMinute();
+
+      resultTime.seconds = 60 - startTime.seconds + endTime.seconds;
+    } else {
+      resultTime.seconds = endTime.seconds - startTime.seconds;
+    }
 
     return resultTime;
   }
